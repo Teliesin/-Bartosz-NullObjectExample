1e7c919e4a800ea23d44c8d298f2b6cf50ee0677
Teliesin <lukasz.manka89@gmail.com>
2019-04-11 13:10:40 +0200

    Add files via upload

---
+++ NullObjectExample/NullObjectExample/NullObjectPawn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObjectExample
+{
+    public class NullObjectPawn : IPawn
+    {
+        public void Move()
+        {
+            Console.WriteLine("Jestem pustym polem!");
+            //throw new ApplicationException("Coś nie poszło nie tak!");
+        }
+    }
+}
---
+++ NullObjectExample/NullObjectExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObjectExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, IPawn> pawns = new Dictionary<string, IPawn>();
+
+            pawns.Add("A1", new Pawn());
+            pawns.Add("A2", new NullObjectPawn());
+
+            IPawn pawn = pawns["A1"];
+            pawn.Move();
+
+            IPawn pawn2 = pawns["A2"];
+            pawn2.Move();
+
+            Console.ReadKey();
+        }
+    }
+}
